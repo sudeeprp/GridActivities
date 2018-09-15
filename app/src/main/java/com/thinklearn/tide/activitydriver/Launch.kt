e401c944280e2a115a6ba71b02d2cbc4e89eeebe
Sudeep <sudeep.rp@gmail.com>
2018-09-15 15:22:44 +0530

    Upload classroom from json, protection for no-school-selection, clear before fill-ing (absentee bug)

--- app/src/main/java/com/thinklearn/tide/activitydriver/Launch.kt
+++ app/src/main/java/com/thinklearn/tide/activitydriver/Launch.kt
@@ -1,5 +1,6 @@
 package com.thinklearn.tide.activitydriver
 
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
@@ -11,58 +12,86 @@ import android.view.WindowManager
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import android.widget.Toast
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.thinklearn.tide.interactor.ClassroomInteractor
 import com.thinklearn.tide.interactor.ClassroomLoaded
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.FileInputStream
+
 
 data class Classroom(val class_name: String, val school_name: String)
 
 class Launch : AppCompatActivity() {
-    lateinit var selected_school_id: String
+    lateinit var selected_class_id: String
     //lateinit var selected_tab_mode: String
 
-    fun get_directory_permission() {
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            println("Directory access permission not granted")
-            ActivityCompat.requestPermissions(this,
-                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                    4)
-        }
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN)
         getSupportActionBar()?.hide();
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
+            launchStartScreen()
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    4)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            launchStartScreen()
+        }
+    }
+    fun launchStartScreen() {
+        selected_class_id = ClassroomInteractor.configureClass()
+        if (selected_class_id == "") {
+            showConfigScreen()
+        } else {
+            loadAndStart(ClassroomInteractor.tab_mode)
+        }
+    }
+    fun loadAndStart(start_mode: String) {
+        if(selected_class_id != "") {
+            ClassroomInteractor.load("ICDev", selected_class_id, object : ClassroomLoaded {
+                override fun onLoadComplete() {
+                    ClassroomInteractor.tab_mode = start_mode
+                    ClassroomInteractor.removeLoadedEvent()
+                    if (ClassroomInteractor.tab_mode == "teacher") {
+                        startTeacherLogin()
+                    } else if (ClassroomInteractor.tab_mode == "student") {
+                        startStudentLogin()
+                    }
+                }
+            })
+        } else {
+            Toast.makeText(this, R.string.need_school_selection, Toast.LENGTH_LONG).show()
+        }
+    }
+    fun showConfigScreen() {
         setContentView(R.layout.activity_launch)
 
         val schoolsListView = findViewById<ListView>(R.id.SchoolsList)
         val thisContext = this
 
-        get_directory_permission()
-
-        val goTeacher = findViewById<Button>(R.id.GoTeacher)
-        goTeacher.setOnClickListener {
-            ClassroomInteractor.load("ICDev", selected_school_id, object: ClassroomLoaded {
-                override fun onLoadComplete() {
-                    ClassroomInteractor.removeLoadedEvent()
-                    startTeacherLogin()
-                }
-            })
+        findViewById<Button>(R.id.GoTeacher).setOnClickListener {
+            loadAndStart("teacher")
         }
-        val goStudent = findViewById<Button>(R.id.GoStudent)
-        goStudent.setOnClickListener {
-            ClassroomInteractor.load("ICDev", selected_school_id, object: ClassroomLoaded {
-                override fun onLoadComplete() {
-                    ClassroomInteractor.removeLoadedEvent()
-                    startStudentLogin()
-                }
-            })
+        findViewById<Button>(R.id.GoStudent).setOnClickListener {
+            loadAndStart("student")
+        }
+        findViewById<Button>(R.id.UploadClassroom).setOnClickListener {
+            uploadClassroom()
         }
 
         //TODO: Check max number of schools (<50) before querying!
@@ -82,10 +111,9 @@ class Launch : AppCompatActivity() {
                         schoolsListView.adapter = adapter
                         schoolsListView.setOnItemClickListener { parent, view, position, id ->
                             println("** #" + position.toString() + " ID = " + school_ids[position] + " is clicked");
-                            selected_school_id = school_ids[position]!!
+                            selected_class_id = school_ids[position]!!
                         }
                     }
-
                     override fun onCancelled(p0: DatabaseError?) {
                         println("ICDev fetch: onCancelled ${p0?.toException()}")
                     }
@@ -95,7 +123,7 @@ class Launch : AppCompatActivity() {
         val teacherLoginIntent = Intent(this, TeacherLoginActivity::class.java)
         teacherLoginIntent.putParcelableArrayListExtra("TEACHER_LIST", ClassroomInteractor.teachers)
         teacherLoginIntent.putExtra("purpose", "PROFILE_EDIT")
-        startActivityForResult(teacherLoginIntent, 0)
+        startActivity(teacherLoginIntent)
     }
     fun startStudentLogin() {
         val studentLoginIntent = Intent(this, GradeSelectionActivity::class.java)
@@ -103,5 +131,37 @@ class Launch : AppCompatActivity() {
         studentLoginIntent.putExtra("purpose", "STUDENT_ACTIVITY")
         studentLoginIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
         startActivity(studentLoginIntent)
+    }
+    fun uploadClassroom() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "*/*"
+        startActivityForResult(intent, 3)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && data != null) {
+            var classroomAndAssetsJSONstr: String = ""
+            val uri = data.getData();
+            val parcelFileDescriptor = contentResolver.openFileDescriptor(uri, "r")
+            val inputStream = FileInputStream(parcelFileDescriptor.fileDescriptor)
+            val buffered = inputStream.bufferedReader()
+
+            var linecount = 0
+            val max_linecount = 15000
+            var line = buffered.readLine()
+            while (line != null && linecount < max_linecount) {
+                classroomAndAssetsJSONstr += line;
+                line = buffered.readLine()
+                linecount += 1
+            }
+            try {
+                val cassetJSON = JSONObject(classroomAndAssetsJSONstr)
+                ClassroomInteractor.uploadClassroom(cassetJSON)
+            } catch(e: JSONException) {
+                Toast.makeText(this, "Error parsing " + uri.toString(), Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
