7e5b34acf6978b107cbdfbc3a29efafc793fe296
Sudeep <sudeep.rp@gmail.com>
2019-01-02 00:03:11 +0530

    Choice of exporting to Bluetooth or SDcard; Kotlin use jdk instead of jre; Update firebase version to 16.1.0

--- app/src/main/java/com/thinklearn/tide/activitydriver/SettingsUtilActivity.kt
+++ app/src/main/java/com/thinklearn/tide/activitydriver/SettingsUtilActivity.kt
@@ -4,21 +4,23 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.net.Uri
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.Toast
 import com.thinklearn.tide.interactor.*
 import java.io.File
 import java.io.IOException
 import android.bluetooth.BluetoothAdapter
+import android.os.Environment
+import android.widget.*
 
 
 class SettingsUtilActivity : AppCompatActivity() {
     var exportFilePath: String = ""
+    var exportChoices = arrayOf ("To sdcard",            "via Bluetooth")
+    var exportRoutines = arrayOf(fun() {sendToSDCard()}, fun() {sendViaBluetooth()})
+    var selectedExportChoiceIndex = -1
+    val exchange_folder = "LearningGridExchange/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,10 +30,19 @@ class SettingsUtilActivity : AppCompatActivity() {
         refresh_status.setOnClickListener { refreshStatus() }
 
         EnvironmentalContext.listenToOnlining(this, { dataConnectionUpdate() })
+        setupExportControls()
+        refreshStatus()
+    }
+    fun setupExportControls() {
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, exportChoices)
+        val exportChoicesView = findViewById<ListView>(R.id.export_destination_choice)
+        exportChoicesView.adapter = adapter
+        exportChoicesView.setOnItemClickListener { _, _, position, _ ->
+            selectedExportChoiceIndex = position
+        }
         findViewById<Button>(R.id.export_data).setOnClickListener {
             exportData()
         }
-        refreshStatus()
     }
     fun refreshStatus() {
         contentVersionUpdate()
@@ -65,22 +76,38 @@ class SettingsUtilActivity : AppCompatActivity() {
     }
     fun exportData() {
         try {
-            exportFilePath = ClassroomDataExchange.exportClassroomData()
-            Toast.makeText(this, resources.getString(R.string.export_success) + exportFilePath, Toast.LENGTH_LONG).show()
-            sendViaBluetooth()
+            if(selectedExportChoiceIndex != -1) {
+                exportFilePath = ClassroomDataExchange.exportClassroomData()
+                Toast.makeText(this, resources.getString(R.string.export_success) + exportFilePath, Toast.LENGTH_LONG).show()
+                exportRoutines[selectedExportChoiceIndex]()
+            } else {
+                Toast.makeText(this, R.string.select_export_dest, Toast.LENGTH_LONG).show()
+            }
         } catch(e: IOException) {
             Toast.makeText(this, resources.getString(R.string.export_failed) + e.localizedMessage, Toast.LENGTH_LONG).show()
+        }
+    }
+    fun sendToSDCard() {
+        val exchangePath = "/storage/sdcard1/"
+        if (File(exchangePath).canWrite()) {
+            val targetFolder = File(exchangePath + exchange_folder)
+            if(targetFolder.exists()) {
+                targetFolder.mkdirs()
+            }
+            File(exportFilePath).copyTo(targetFolder, true)
+        } else {
+            Toast.makeText(this, R.string.no_sdcard, Toast.LENGTH_LONG).show()
         }
     }
     fun sendViaBluetooth() {
         val btAdapter = BluetoothAdapter.getDefaultAdapter()
         val DISCOVER_DURATION = 300
-        if (btAdapter == null) {
-            Toast.makeText(this, "Bluetooth is not supported on this device", Toast.LENGTH_LONG).show()
-        } else {
+        if (btAdapter != null) {
             val discoveryIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
             discoveryIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, DISCOVER_DURATION)
             startActivityForResult(discoveryIntent, 3)
+        } else {
+            Toast.makeText(this, "Bluetooth is not supported on this device", Toast.LENGTH_LONG).show()
         }
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -92,27 +119,14 @@ class SettingsUtilActivity : AppCompatActivity() {
             intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(File(exportFilePath)))
 
             val appsList = packageManager.queryIntentActivities(intent, 0)
+                    .filter{ it.activityInfo.packageName == "com.android.bluetooth" }
 
             if (appsList.size > 0) {
-                var packageName: String = ""
-                var className: String = ""
-                var found = false
-
-                for (info in appsList) {
-                    packageName = info.activityInfo.packageName
-                    if (packageName == "com.android.bluetooth") {
-                        className = info.activityInfo.name
-                        found = true
-                        break
-                    }
-                }
-                if (!found) {
-                    Toast.makeText(this, "Bluetooth app isn't found",
-                            Toast.LENGTH_LONG).show()
-                } else {
-                    intent.setClassName(packageName, className)
-                    startActivity(intent)
-                }
+                intent.setClassName(appsList[0].activityInfo.packageName, appsList[0].activityInfo.name)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Bluetooth app isn't found",
+                        Toast.LENGTH_LONG).show()
             }
         }
     }
