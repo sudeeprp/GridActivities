d0ea5019745604f1af122945423db26fe9431b53
Sudeep <sudeep.rp@gmail.com>
2019-02-14 21:31:20 +0530

    Added Goa database, removed gender selection in small class, multi-class upload

--- app/src/main/java/com/thinklearn/tide/activitydriver/StudentGridActivity.java
+++ app/src/main/java/com/thinklearn/tide/activitydriver/StudentGridActivity.java
@@ -35,11 +35,11 @@ public class StudentGridActivity extends AppCompatActivity implements StudentGri
         String selectedGender = getIntent().getStringExtra("selectedGender");
         String displayGrade = getString(
                 getResources().getIdentifier("grade" + selectedGrade,"string", getPackageName()));
-        ((TextView) findViewById(R.id.selectedClass)).setText(": " +displayGrade);
+        ((TextView) findViewById(R.id.selectedClass)).setText(displayGrade);
         if(!selectedGender.isEmpty()) {
             String displayGender = getString(
                     getResources().getIdentifier(selectedGender, "string", getPackageName()));
-            ((TextView) findViewById(R.id.selectedGender)).setText(": " + displayGender);
+            ((TextView) findViewById(R.id.selectedGender)).setText(displayGender);
         }
         findViewById(R.id.loginButton).setEnabled(false);
 
